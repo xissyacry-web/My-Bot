9dfab9aba9823081b0b19c75dc62afeed6a29214
xissyacry-web <xissyacry@gmail.com>
2026-05-30 12:40:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-BOT_TOKEN = "8961635368:AAGrLICFaRDceOFDa5RBIlY2274_DKtvs0k"            # Токен основного бота
-CRYPTO_BOT_TOKEN = "588982:AARxXJtGOMKkXibK6z9yOogblUSEYerHJqD"  # Токен из @CryptoBot или @CryptoTestnetBot
-ADMIN_IDS = [1073780833]                 # Telegram ID администраторов
-DATABASE_URL = "sqlite+aiosqlite:///bot.db"
+BOT_TOKEN = "8961635368:AAGrLICFaRDceOFDa5RBIlY2274_DKtvs0k"
+CRYPTO_BOT_TOKEN = "588982:AARxXJtGOMKkXibK6z9yOogblUSEYerHJqD"
+ADMIN_IDS = [1073780833]
+DATABASE_URL = "sqlite+aiosqlite:///./bot.db"
+BOT_USERNAME = "XissyaLog_Bot"          # <-- укажи юзернейм своего бота без @
