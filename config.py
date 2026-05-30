b128d156a58e860e7f44cebe4d83d480317e9163
xissyacry-web <xissyacry@gmail.com>
2026-05-30 14:48:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "8961635368:AAGrLICFaRDceOFDa5RBIlY2274_DKtvs0k"
 CRYPTO_BOT_TOKEN = "588982:AARxXJtGOMKkXibK6z9yOogblUSEYerHJqD"
-ADMIN_IDS = [1073780833]
+ADMIN_IDS = [1073780833, 8704427047]
 DATABASE_URL = "sqlite+aiosqlite:///./bot.db"
 BOT_USERNAME = "XissyaLog_Bot"          # <-- укажи юзернейм своего бота без @
