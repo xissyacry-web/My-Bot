c451f94ae4fdd398f6322fe82c7845edf3bbf12b
xissyacry-web <xissyacry@gmail.com>
2026-06-11 22:27:32 +0500

    Update reply.py

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,18 +1,18 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-def get_main_reply_keyboard():
+def main_menu():
+    """Главное reply-меню бота"""
     builder = ReplyKeyboardBuilder()
     
-    # Вставляем кастомные эмодзи прямо как символы из пака!
     builder.row(
-        KeyboardButton(text="📦 Каталог"),
+        KeyboardButton(text="📋 Каталог"),
         KeyboardButton(text="👤 Профиль")
     )
     builder.row(
         KeyboardButton(text="♻️ Замена"),
         KeyboardButton(text="🆘 Поддержка"),
-        KeyboardButton(text="🏷️ Скидка")
+        KeyboardButton(text="🏷 Скидка")
     )
     
     return builder.as_markup(resize_keyboard=True)
