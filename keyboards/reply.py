0202d18804c208a5492fcd2524b32d2c30bc823c
xissyacry-web <xissyacry@gmail.com>
2026-06-12 09:54:21 +0500

    Update reply.py

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,9 +1,8 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from config import pe
 
 def main_menu():
     return ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text=f"{pe('folder')} Каталог"),   KeyboardButton(text=f"{pe('user')} Профиль")],
-        [KeyboardButton(text=f"{pe('info')} Поддержка"),   KeyboardButton(text=f"{pe('hammer')} Замена")],
-        [KeyboardButton(text=f"{pe('star')} Скидка")],
+        [KeyboardButton(text="📋 Каталог"),   KeyboardButton(text="👤 Профиль")],
+        [KeyboardButton(text="🆘 Поддержка"), KeyboardButton(text="♻️ Замена")],
+        [KeyboardButton(text="🏷 Скидка")],
     ], resize_keyboard=True)
